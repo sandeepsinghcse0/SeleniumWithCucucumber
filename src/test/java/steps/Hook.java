e2e54e64f2f7ab521fa8ffedcc41d43e6a263fe5
SANDEEP SINGH <sandeepsinghcse0@gmail.com>
2019-08-19 23:49:37 +0530

    Corrected path seprater

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -31,7 +31,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\Libs\chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:/Libs/Chromedriver");
         base.Driver = new ChromeDriver();
     }
 
